43cbd84576c07eaf610f61a37831bf98b008a56b
Dana Nurymbetova <dnurym567@gmail.com>
2022-09-07 20:00:37 +0600

    first com

---
+++ js/my.js
@@ -0,0 +1,27 @@
+let a = 10;
+let b = 2;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+let c = 15;
+let d = 2;
+let result = c + d;
+console.log('first res ' + result);
+
+a = 17;
+b = 10;
+c = a - b;
+d = 7;
+result = c + d;
+console.log('second res ' + result);
+
+let str = 'Привет, Мир!.';
+console.log(str);
+
+// let a = prompt("Первое число?", 1);
+// let b = prompt("Второе число?", 2);
+
+// console.log(Number(a) + Number(b));

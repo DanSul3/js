12df3f190b203c3b5f6b7d96fc37c575b1939e08
Dana Nurymbetova <dnurym567@gmail.com>
2022-09-09 20:03:14 +0600

    first com

--- js/my.js
+++ js/my.js
@@ -1,27 +1,140 @@
-let a = 10;
-let b = 2;
+// let name = "Ilya";
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
+// alert( `hello ${1}` );
 
-let c = 15;
-let d = 2;
-let result = c + d;
-console.log('first res ' + result);
+// alert( `hello ${"name"}` );
 
-a = 17;
-b = 10;
-c = a - b;
-d = 7;
-result = c + d;
-console.log('second res ' + result);
+// alert( `hello ${name}` );
 
-let str = 'Привет, Мир!.';
-console.log(str);
+// let lastN = prompt('вводите фамилию', '');
+// alert(`ваша фамилия ${lastN}`)
 
-// let a = prompt("Первое число?", 1);
-// let b = prompt("Второе число?", 2);
+"" + 1 + 0 // 10
+console.log("" + 1 + 0) // +
 
-// console.log(Number(a) + Number(b));
+"" - 1 + 0 // -1
+console.log("" - 1 + 0) // +
+
+true + false // 1
+console.log(true + false) // +
+
+6 / "3" // 2
+console.log(6 / "3") // +
+
+"2" * "3" // 6
+console.log("2" * "3") // +
+
+4 + 5 + "px" //9px
+console.log(4 + 5 + "px")
+
+"4" - 2 // 2
+console.log("4" - 2) // +
+
+"4px" - 2 // 4px -2
+console.log("4px" - 2) //nan
+
+"  -9  " + 5 // -9 5
+console.log("  -9  " + 5) // +
+
+"  -9 " - 5 // -14
+console.log("  -9  " - 5) // +
+
+null + 1 // 1
+console.log(null + 1) // +
+
+undefined + 1 // --
+console.log(undefined + 1) //nan
+
+" \t \n" - 2 // t n -2
+console.log(" \t \n" - 2) // -2
+
+let num = 3;
+console.log(num * num);
+
+//1
+console.log(60 * 60) // в часе 
+console.log(60 * 60 * 24); // в сутках
+console.log(60 * 60 * 24 * 31) // в месяце
+
+
+//2
+// let age = 12;
+// if(age >= 14 && age <= 90){
+//     alert('in the range')
+// }
+// else if(!(age >= 14 && age <= 90)){
+//     alert('not in the range')
+// }
+// else{
+//     alert('just in case')
+// }
+
+
+
+
+
+//3
+// let enterLogin = prompt('enter login', '');
+// if(enterLogin === 'admin'){
+//     let password = prompt('enter password', '');
+//     if(password === `I'm the boss`){
+//         alert('Здравствуйте!')
+//     }
+//     else if(password === null){
+//         alert('Отменено')
+//     }
+//     else{
+//         alert('Неверный пароль')
+//     }
+
+// }else if(enterLogin === null){
+//     alert('Отменено');
+// }
+// else{
+//     alert('Я вас не знаю')
+// }
+
+
+
+//4
+num = 5;
+// if(isNaN(num)){
+//     alert('Variable is not a number');
+// }
+// else{
+//     alert('Variable is a valid number')
+// }
+
+
+
+
+//5
+let num1 = 55;
+let num2 = 66;
+
+// if(num1 > num2){
+//     alert('num1 largest')
+// }
+// else if(num1 == num2){
+//     alert ('equal')
+// }else{
+//     alert ('num2 largest')
+// }
+
+
+
+//6
+let num3 = 3;
+
+if (num1 > num2 && num1 > num3) {
+    alert('num1 largest')
+}
+else if (num1 < num2 && num2 > num3) {
+    alert('num2 largest')
+} else {
+    alert('3')
+}
+
+
+
+
